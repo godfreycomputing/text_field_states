fc4b3697f187a93845b6df054945820c58275a43
godfreycomputing <godfreycomputing@gmail.com>
2022-02-22 07:50:46 -0500

    Corrected example

--- example/example.dart
+++ example/example.dart
@@ -2,70 +2,95 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 import 'package:generic_text_field_states/generic_text_field_states.dart';
 
+final emailProvider =
+    StateNotifierProvider<TextFieldStateProvider, TextFieldState>(
+        (ref) => TextFieldStateProvider(TextFieldState.initial()));
+final passwordProvider =
+    StateNotifierProvider<TextFieldStateProvider, TextFieldState>(
+        (ref) => TextFieldStateProvider(TextFieldState.initial()));
+
+Future<void> signIn({required String password, required String email}) async {
+  //Signed in
+}
+
 void main() {
   runApp(MainApp());
 }
 
-class MainApp extends ConsumerWidget {
+class MainApp extends StatelessWidget {
   const MainApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final textFieldNotifier = ref.read(textFieldStatesProvider.notifier);
-    final textFieldState = ref.watch(textFieldStatesProvider);
+  Widget build(BuildContext context) {
     return Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text(textFieldState.when(
-            initial: () => '',
-            valid: (value) => value,
-            invalid: (error) => error,
-          )),
-          TextField(
-            onChanged: (value) => textFieldNotifier.changeValue(
-                fieldType: FieldType.string(),
-                value: value,
-                valueName: 'First Name'),
-            decoration: InputDecoration(
-              labelText: 'First Name',
-              errorText: textFieldState.maybeWhen(
-                orElse: () => null,
-                invalid: (error) => error,
-              ),
-            ),
-          ),
-          TextField(
-            onChanged: (value) => textFieldNotifier.changeValue(
-              fieldType: FieldType.num(),
-              value: value,
-              valueName: 'Age',
-            ),
-            decoration: InputDecoration(
-              labelText: 'Age',
-              errorText: textFieldState.maybeWhen(
-                orElse: () => null,
-                invalid: (error) => error,
-              ),
-            ),
-          ),
-          TextField(
-            onChanged: (value) => textFieldNotifier.changeValue(
-              fieldType: FieldType.email(),
-              value: value,
-              valueName: 'Email Address',
-            ),
-            decoration: InputDecoration(
-              labelText: 'Email Address',
-              errorText: textFieldState.maybeWhen(
-                orElse: () => null,
-                invalid: (error) => error,
-              ),
-            ),
-          ),
-        ],
+        child: Column(
+      children: [
+        GenericTextField(
+          provider: emailProvider,
+          fieldType: FieldType.email(),
+          valueName: 'Email Address',
+        ),
+        GenericTextField(
+          provider: passwordProvider,
+          fieldType: FieldType.password(),
+          valueName: 'Password',
+        ),
+      ],
+    ));
+  }
+}
+
+class GenericTextField extends ConsumerWidget {
+  final StateNotifierProvider<TextFieldStateProvider, TextFieldState> provider;
+  final FieldType fieldType;
+  final String valueName;
+
+  const GenericTextField({
+    Key? key,
+    required this.provider,
+    required this.fieldType,
+    required this.valueName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final providerNotifier = ref.read(provider.notifier);
+    final providerState = ref.watch(provider);
+    return TextField(
+      onChanged: (value) => providerNotifier.changeValue(
+        fieldType: fieldType,
+        value: value,
+        valueName: valueName,
       ),
+      decoration: InputDecoration(
+        errorText: providerState.maybeWhen(
+          orElse: () => null,
+          invalid: (error) => error,
+        ),
+      ),
+    );
+  }
+}
+
+class LoginButton extends ConsumerWidget {
+  const LoginButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final emailState = ref.watch(emailProvider);
+    final passwordState = ref.watch(passwordProvider);
+    return ElevatedButton(
+      onPressed: () => signIn(
+        password: passwordState.maybeWhen(
+          orElse: () => '',
+          valid: (value) => value,
+        ),
+        email: emailState.maybeWhen(
+          orElse: () => '',
+          valid: (value) => value,
+        ),
+      ),
+      child: Text('Submit'),
     );
   }
 }
